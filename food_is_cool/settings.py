685eafeef9c2483ff43d83c418a8516745f74e4e
David <dhblodgett@gmail.com>
2016-04-28 12:56:44 -0700

    staticfiles

--- food_is_cool/settings.py
+++ food_is_cool/settings.py
@@ -134,8 +134,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+# MEDIA_URL = "/media/"
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/home'
