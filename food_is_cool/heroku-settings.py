b60707e73aa043049e171b25a7a2c3a6c4ec4275
David <dhblodgett@gmail.com>
2016-04-28 12:54:34 -0700

    staticfiles

--- food_is_cool/heroku-settings.py
+++ food_is_cool/heroku-settings.py
@@ -1,24 +1,25 @@
-import dj_database_url
 from .settings import *
+import os
 
 DEBUG=False
-SECRET_KEY = os.environ["SECRET_KEY"]
+SECRET_KEY = os.environ['SECRET_KEY']
 
-# Database
-DATABASES["default"] = dj_database_url.config()
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-ALLOWED_HOSTS = ["*"]
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
 
-# Static Configuration
+# Static asset configuration
+import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_URL = "/static/"
 STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, '../global/'),
-# )
-
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, '../global/'),
+)
