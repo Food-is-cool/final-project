12fc58a1a236860b0d42b8745a361dff97c6791e
David <dhblodgett@gmail.com>
2016-05-03 10:12:58 -0700

    reset Heroku database and remigrated

--- food_is_cool/urls.py
+++ food_is_cool/urls.py
@@ -22,11 +22,7 @@ from django.contrib.auth.forms import UserCreationForm
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url('^register/', CreateView.as_view(template_name='register.html',
-                                         form_class=UserCreationForm,
-                                         success_url='/'
-    )),
-url(r'^api-token-auth/', obtain_auth_token),
+    url(r'^api-token-auth/', obtain_auth_token),
     url(r'^api/', include('mainsite.urls_api')),
     url(r'^docs/', include('rest_framework_swagger.urls')),
     url('^', include('django.contrib.auth.urls')),
